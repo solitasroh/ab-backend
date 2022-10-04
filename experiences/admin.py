0b73f5a3cf247b190ed18552d7839551336c1ac9
solitasroh <78634894+solitasroh@users.noreply.github.com>
2022-10-04 22:15:37 +0900

    6.5 Experiences

---
+++ experiences/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from experiences.models import Experience, Perk
+
+# Register your models here.
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "price",
+        "start",
+        "end",
+        "created_at",
+    )
+
+
+@admin.register(Perk)
+class PerkAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "details",
+        "explanation",
+    )
---
+++ experiences/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+from common.models import ModelMixin
+
+# Create your models here.
+
+
+class Experience(ModelMixin):
+
+    """Experience Model Definiiton"""
+
+    country = models.CharField(
+        max_length=50,
+        default="한국",
+    )
+    city = models.CharField(
+        max_length=80,
+        default="서울",
+    )
+    name = models.CharField(
+        max_length=250,
+    )
+    host = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    price = models.PositiveIntegerField()
+    address = models.CharField(
+        max_length=250,
+    )
+    start = models.TimeField()
+    end = models.TimeField()
+    description = models.TextField()
+    perks = models.ManyToManyField(
+        "experiences.Perk",
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Perk(ModelMixin):
+
+    """What is included on an Experience"""
+
+    name = models.CharField(
+        max_length=100,
+    )
+    details = models.CharField(
+        max_length=250,
+        blank=True,
+        default="",
+    )
+    explanation = models.TextField(
+        blank=True,
+        default="",
+    )
+
+    def __str__(self) -> str:
+        return self.name
