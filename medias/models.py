594de6f842c567e0ef3df0a622014ce08c6b0576
solitasroh <78634894+solitasroh@users.noreply.github.com>
2022-10-07 21:22:00 +0900

    6.10 medias - photo -- file, description, room, experience
    - video
    - file, experience

---
+++ medias/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+from common.models import ModelMixin
+
+
+class Photo(ModelMixin):
+
+    file = models.ImageField()
+    description = models.CharField(
+        max_length=140,
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return "Photo File"
+
+
+class Video(ModelMixin):
+
+    file = models.FileField()
+    experience = models.OneToOneField(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "Video File"
