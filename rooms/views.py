0546b02f5eaa9b91f99491f0679f1a3b2d71e3c4
solitasroh <78634894+solitasroh@users.noreply.github.com>
2022-10-31 21:00:00 +0900

    9.2 url arguments path converter를 사용

--- rooms/views.py
+++ rooms/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("hello!")
+def see_all_rooms(request):
+    return HttpResponse("see all rooms")
+
+
+def see_one_room(request, room_id: int):
+    print(room_id)
+    return HttpResponse("see one room")
